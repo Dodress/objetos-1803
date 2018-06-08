25820afc086f033651563d12cea46715d2a6686d
T-101 <T-101@PC223>
2018-06-07 19:39:20 -0500

    aqui implementams ArrayList

---
+++ objetos-1803/src/genericos/Pregunta.java
@@ -0,0 +1,26 @@
+
+package genericos;
+
+import java.util.*;
+public class Pregunta {
+    private String titulo;
+    private ArrayList<Opcion> opcion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public ArrayList<Opcion> getOpcion() {
+        return opcion;
+    }
+
+    public void setOpcion(ArrayList<Opcion> opcion) {
+        this.opcion = opcion;
+    }
+    
+    
+}
