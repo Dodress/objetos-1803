55111b91469c5c10ef9f8ee67cc392b472f1d230
ivan <ivan@ivan-PC.lan>
2018-06-22 02:14:31 -0500

    jugue un poco con los constructores para comprobar que era posible y que no se se tenia permitido hacer

---
+++ objetos-1803/src/arrays/Arreglo.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Arreglo {
+    public static void main(String[] args){
+     String s;
+     s="hola";
+        System.out.println(s.length());
+     s="programacion";
+        System.out.println(s.length());
+     s+="hola";
+        System.out.println(s.length());
+        System.out.println(s[3]);
+    }
+    
+}
