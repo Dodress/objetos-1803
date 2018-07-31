17641484eac2c3cb3e1f45aef7774362ded1de65
T-101 <T-101@PC223>
2018-07-30 19:48:59 -0500

    mostrar preguntas y el temporizador

--- ProyectoFinal/src/vista/TestAplicacion.java
+++ ProyectoFinal/src/vista/TestAplicacion.java
@@ -5,11 +5,20 @@
  */
 package vista;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.PersistenciaPregunta;
+import modelo.*;
+
 /**
  *
  * @author T-101
  */
 public class TestAplicacion extends javax.swing.JFrame {
+
+    int numero = 0;
+    int tiempo = 0;
 
     /**
      * Creates new form TestAplicacion
@@ -17,6 +26,49 @@ public class TestAplicacion extends javax.swing.JFrame {
     public TestAplicacion() {
         initComponents();
         setLocationRelativeTo(this);
+        setSize(550, 300);
+        mostrarPregunta(numero++);
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    tiempo++;
+                    try {
+                        Thread.sleep(1000);
+                        etiquetaTiempo.setText("" + tiempo);
+                        if (tiempo >= 30) {
+                            mostrarPregunta(numero++);
+                        }
+
+                    } catch (InterruptedException ex) {
+                    }
+                }
+            }
+        });
+        t1.start();
+    }
+
+    public void mostrarPregunta(int numero) {
+        tiempo = 0;
+        try {
+            //primero sacamos la pregunta del numero dado
+            ArrayList<Pregunta> preguntas = PersistenciaPregunta.leer();
+            Pregunta p = preguntas.get(numero);
+            //ajustams los valores
+            //primero va el titulo
+            etiquetaPregunta.setText(p.getTitulo());
+            //ahora las opciones
+            ArrayList<Opcion> opciones = p.getOpcion();
+            //Aplicamos el algoritmo
+            opciones = PersistenciaPregunta.opcionesAleatorias(opciones);
+            rb1.setText(opciones.get(0).getTitulo());
+            rb2.setText(opciones.get(1).getTitulo());
+            rb3.setText(opciones.get(2).getTitulo());
+            rb4.setText(opciones.get(3).getTitulo());
+        } catch (Exception ex) {
+
+        }
+
     }
 
     /**
@@ -28,21 +80,70 @@ public class TestAplicacion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLabel1 = new javax.swing.JLabel();
+        etiquetaTiempo = new javax.swing.JLabel();
+        etiquetaPregunta = new javax.swing.JLabel();
+        rb1 = new javax.swing.JRadioButton();
+        rb2 = new javax.swing.JRadioButton();
+        rb3 = new javax.swing.JRadioButton();
+        rb4 = new javax.swing.JRadioButton();
+        jButton1 = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setText("Contestar pregunta");
+        getContentPane().add(jLabel1);
+
+        etiquetaTiempo.setText("TIEMPO");
+        getContentPane().add(etiquetaTiempo);
+
+        etiquetaPregunta.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        etiquetaPregunta.setText("Aqui ira la pregunta");
+        getContentPane().add(etiquetaPregunta);
+
+        buttonGroup1.add(rb1);
+        rb1.setText("jRadioButton1");
+        getContentPane().add(rb1);
+
+        buttonGroup1.add(rb2);
+        rb2.setText("jRadioButton2");
+        getContentPane().add(rb2);
+
+        buttonGroup1.add(rb3);
+        rb3.setText("jRadioButton3");
+        getContentPane().add(rb3);
+
+        buttonGroup1.add(rb4);
+        rb4.setText("jRadioButton4");
+        rb4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rb4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(rb4);
+
+        jButton1.setText("Siguiente pregunta >>");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void rb4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rb4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rb4ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        mostrarPregunta(numero++);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +181,14 @@ public class TestAplicacion extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JLabel etiquetaPregunta;
+    private javax.swing.JLabel etiquetaTiempo;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JRadioButton rb1;
+    private javax.swing.JRadioButton rb2;
+    private javax.swing.JRadioButton rb3;
+    private javax.swing.JRadioButton rb4;
     // End of variables declaration//GEN-END:variables
 }
